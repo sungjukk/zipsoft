f851c68069c67f9f73bb125e0684a1e372739a6c
User <User@175.195.57.168>
2023-08-10 18:12:52 +0900

    최초

---
+++ zipSoft/src/main/java/com/zipsoft/ZipSoftApplication.java
@@ -0,0 +1,13 @@
+package com.zipsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipSoftApplication.class, args);
+	}
+
+}
---
+++ zipSoft/src/test/java/com/zipsoft/ZipSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
