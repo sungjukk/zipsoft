a66cc43cd3661bc69681ea0f2ea7089265aa8233
sungjukk <lsj0147@naver.com>
2023-09-19 21:49:04 +0900

    레이아웃 수정

--- zipSoft/frontend/src/components/layouts/PageHeader.vue
+++ zipSoft/frontend/src/components/layouts/PageHeader.vue
@@ -1,24 +1,61 @@
 <template>
 	<header>
-		<nav class="navbar navbar-dark bg-dark fixed-top">
-			<div class="container-fluid">
+		<nav class="navbar navbar-dark fixed-top page-nav" style="background-color : #249d6a">
+			<div class="container-fluid page-header">
 				<div>
-					<button class="navbar-toggler" type="button" @click="openSidebar()">
+					<!--<button class="navbar-toggler" type="button" @click="openSidebar()">
 						<span class="navbar-toggler-icon"></span>
-					</button>
-					<a class="navbar-brand" href="#" style="margin-left: 20px;">{{ title }}</a>
+					</button>-->
+					<a class="navbar-brand nav-font" href="#" >{{ title }}</a>
+					<div class="pc-menu-list">
+						<div class="menu-box">
+							<div class="menu-btn-div">
+								<a class="nav-font menu-hover">Friend</a>
+							</div>
+							<div class="menu-btn-div">
+								<a class="nav-font menu-hover">Chat</a>
+							</div>
+							<div class="menu-btn-div">
+								<a class="nav-font menu-hover">Board</a>
+							</div>
+						</div>
+					</div>
 				</div>
+
 				<div>
-					<div class="navbar-font" v-if="$store.getters['UserStore/isLogin']">
+
+					<div class="nav-font" v-if="$store.getters['UserStore/isLogin']">
 						<span >{{$store.state.UserStore.name}}님 </span>
 						<a href="javascript:;"  @click="logout">로그아웃</a>
 					</div>
-					<router-link class="navbar-font" :to="`${RouteUrl.LOGIN}`" v-else>로그인</router-link>
+					<router-link class="nav-font" :to="`${RouteUrl.LOGIN}`" v-else>로그인</router-link>
 				</div>
 			</div>
 			<SideMenu ref="sideBar" :title="title"/>
 		</nav>
 	</header>
+	<div class="menuBar">
+		<div class="menu-list">
+			<div class="menu-box">
+				<div class="menu-btn-div menu-hover">
+					<i class="bi bi-people-fill menu-icon"></i>
+					<p class="menu-description">Friend</p>
+				</div>
+			</div>
+			<div class="menu-box">
+				<div class="menu-btn-div menu-hover">
+					<i class="bi bi-chat-fill menu-icon"></i>
+					<p class="menu-description">Chat</p>
+				</div>
+			</div>
+			<div class="menu-box">
+				<div class="menu-btn-div menu-hover">
+					<i class="bi bi-card-list menu-icon"></i>
+					<p class="menu-description">Board</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
